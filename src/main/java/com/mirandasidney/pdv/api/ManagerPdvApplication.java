b6c460fddebdfced187b50944d2f47433286bb58
Sidney Miranda <sidneysmiranda@hotmail.com>
2021-10-26 20:19:42 -0300

    Geração do projeto pelo Spring

---
+++ src/main/java/com/mirandasidney/pdv/api/ManagerPdvApplication.java
@@ -0,0 +1,13 @@
+package com.mirandasidney.pdv.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerPdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerPdvApplication.class, args);
+	}
+
+}
